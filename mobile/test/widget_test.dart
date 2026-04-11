389293383bb6e43b6083858c36e106affbe33ce3
Mimoun Tigunit <mimountigunit@gmail.com>
2026-04-11 07:27:58 +0100

    chore: enable very_good_analysis package in analysis options

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/main.dart';
 
 void main() {
-  testWidgets('MyApp shows Music Room text', (WidgetTester tester) async {
+  testWidgets('MyApp shows Music Room text', (tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.text('Music Room'), findsOneWidget);
