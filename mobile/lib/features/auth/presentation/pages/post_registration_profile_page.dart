208102f82cb5ca5f083104a21952fdbc9e8c94c4
Mimoun Tigunit <mimountigunit@gmail.com>
2026-05-22 23:44:18 +0100

    fix(post_registration_profile): remove unused import for flutter foundation

--- mobile/lib/features/auth/presentation/pages/post_registration_profile_page.dart
+++ mobile/lib/features/auth/presentation/pages/post_registration_profile_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
