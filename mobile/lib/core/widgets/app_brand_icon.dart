20260b3e80c076adfe1d5d0f94da6540b8d768be
Mimoun Tigunit <mimountigunit@gmail.com>
2026-05-25 00:44:17 +0100

    fix(icons): render brand icon with ImageIcon for theme tinting

--- mobile/lib/core/widgets/app_brand_icon.dart
+++ mobile/lib/core/widgets/app_brand_icon.dart
@@ -6,19 +6,20 @@ class AppBrandIcon extends StatelessWidget {
   const AppBrandIcon({
     this.size = 24,
     this.assetPath = 'assets/icon/brand_icon.png',
+    this.color,
     super.key,
   });
 
   final double size;
   final String assetPath;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      assetPath,
-      width: size,
-      height: size,
-      fit: BoxFit.contain,
+    return ImageIcon(
+      AssetImage(assetPath),
+      size: size,
+      color: color,
     );
   }
 }
